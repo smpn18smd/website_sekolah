1de9e29ed331184ffea632a81d67e8b01fba3d9f
smpn18smd <smpn.18.smd@gmail.com>
2026-05-20 23:21:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
 );
 
 
