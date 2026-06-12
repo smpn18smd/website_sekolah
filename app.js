72a6c702b4c47660cf22f7e2d87b7c3cd186d171
smpn18smd <smpn.18.smd@gmail.com>
2026-06-12 14:56:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -72,20 +72,6 @@ alert(
 );
 await loadStatistikSiswa();
 setAdminTab('statistik');
-
-
-  
-console.log('ID AKTIF', statistikSiswa.id);
-const { data: hasil, error } =
-await supabaseClient
-.from('statistik_siswa')
-.update(data)
-.eq('id', statistikSiswa.id)
-.select();
-console.log('HASIL', hasil);
-console.log('ERROR', error);
-
-
   
 }
 
